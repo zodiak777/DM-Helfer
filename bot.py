476cac6bbec59f01273517a96197da282ba70fa1
zodiak777 <sebastian087@hotmail.de>
2025-07-26 19:43:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    print(message)
+    print(message.content)
     if message.author == client.user:
         return
     if message.author.bot:
